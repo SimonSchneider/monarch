e4692fa2e5886e533e34c79cb4a720f0e7682e8b
Simon Schneider <dev@simonschneider.org>
2020-05-10 16:01:34 +0200

    wip

--- services/monarch-ui/src/App.js
+++ services/monarch-ui/src/App.js
@@ -1,43 +1,33 @@
 import React from "react";
 import TopologyGraph from "./TopologyGraph";
 import Configuration from "./Configuration";
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import { Route, NavLink, HashRouter } from "react-router-dom";
 import styles from "./grid.module.css";
-import TimelineIcon from '@material-ui/icons/Timeline';
-import SettingsIcon from '@material-ui/icons/Settings';
-import RefreshIcon from '@material-ui/icons/Refresh';
+import TimelineIcon from "@material-ui/icons/Timeline";
+import SettingsIcon from "@material-ui/icons/Settings";
+import RefreshIcon from "@material-ui/icons/Refresh";
 import { useDispatch, useSelector } from "react-redux";
-import { LOAD_CONFIG, LOAD_BACKEND_CONFIG } from "./redux/actionTypes";
+import actions from "./requests";
 import schedule from "./scheduler";
 
 const Content = (loaded) => {
   if (!loaded) {
-    return (<div className={styles.contentWrapper}>LOADING STUFF</div>)
+    return <div className={styles.contentWrapper}>LOADING STUFF</div>;
   }
-  return (<div className={styles.contentWrapper}>
-    <Route exact path="/" component={TopologyGraph} />
-    <Route exact path="/graph" component={TopologyGraph} />
-    <Route path="/config" component={Configuration} />
-  </div>);
-}
+  return (
+    <div className={styles.contentWrapper}>
+      <Route exact path="/" component={TopologyGraph} />
+      <Route exact path="/graph" component={TopologyGraph} />
+      <Route path="/config" component={Configuration} />
+    </div>
+  );
+};
 
-const App = () => {
+export default () => {
   const loaded = useSelector((state) => state.config.loaded);
   const loadedBe = useSelector((state) => state.backendConfig.loaded);
   const dispatch = useDispatch();
-
-  const update = async () => {
-    const fromBe = await fetch(`/api/v1/curr`).then((r) => r.json());
-    dispatch({ type: LOAD_CONFIG, payload: fromBe });
-  };
-  const updateBe = async () => {
-    const fromBe = await fetch(`/api/v1/configurations`).then((r) => r.json());
-    dispatch({ type: LOAD_BACKEND_CONFIG, payload: fromBe });
-  }
+  const { update, updateBe } = actions(dispatch);
 
   if (!loaded) {
     schedule("update", 15000, update);
@@ -51,17 +41,22 @@ const App = () => {
       <div className={styles.grid}>
         <div className={styles.sidebar}>
           <div>
-            <NavLink to="/graph"><TimelineIcon className={styles.sidebarIcon} /></NavLink>
-            <NavLink to="/config"><SettingsIcon className={styles.sidebarIcon} /></NavLink>
+            <NavLink to="/graph">
+              <TimelineIcon className={styles.sidebarIcon} />
+            </NavLink>
+            <NavLink to="/config">
+              <SettingsIcon className={styles.sidebarIcon} />
+            </NavLink>
           </div>
           <div className={styles.bottomIcons}>
-            <RefreshIcon onClick={() => update()} className={styles.sidebarIcon} />
+            <RefreshIcon
+              onClick={() => update()}
+              className={styles.sidebarIcon}
+            />
           </div>
         </div>
         {Content(loaded && loadedBe)}
       </div>
-    </HashRouter >
+    </HashRouter>
   );
 };
-
-export default App;
--- services/monarch-ui/src/Configuration.js
+++ services/monarch-ui/src/Configuration.js
@@ -1,22 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
 import gridStyles from "./grid.module.css";
-import JSONInput from 'react-json-editor-ajrm';
-import locale from 'react-json-editor-ajrm/locale/en';
+import JSONInput from "react-json-editor-ajrm";
+import locale from "react-json-editor-ajrm/locale/en";
+import { useDispatch, useSelector } from "react-redux";
+import actions from "./requests";
 
-const Configuration = () => {
+export default () => {
   const config = useSelector((state) => state.backendConfig.config);
+  const dispatch = useDispatch();
+  const { newConfig } = actions(dispatch);
+  const [localConfig, setLocalConfig] = useState(config);
   return (
-    <div className={gridStyles.content} style={{ overflow: "justified" }} >
+    <div className={gridStyles.content} style={{ overflow: "justified" }}>
+      <button onClick={() => newConfig(localConfig)}>Gurka</button>
       <JSONInput
-        placeholder={config}
+        placeholder={localConfig}
         locale={locale}
-        waitAfterKeyPress={2000}
+        waitAfterKeyPress={1000}
         height="100%"
         width="100%"
+        onChange={(c) => setLocalConfig(c.jsObject)}
       />
     </div>
   );
 };
-
-export default Configuration;
