8482cfd2d4de0fa8ed3d6cd4cc37bb16d466c18a
Simon Schneider <dev@simonschneider.org>
2020-07-05 15:46:00 +0200

    basic list

--- services/monarch-ui/src/VisualConfig.js
+++ services/monarch-ui/src/VisualConfig.js
@@ -6,9 +6,29 @@ const VisualConfig = () => {
   if (!config.loaded) {
     return <div style={{ overflow: "justified" }}>Loading...</div>;
   }
+  console.log(config);
   return (
     <div style={{ overflow: "justified" }}>
-      {JSON.stringify(config, undefined, 2)}
+      <div>
+        Services:
+        {config.config.services.map((s) => (
+          <div key={`visual-config-service-${s.name}`}>{JSON.stringify(s)}</div>
+        ))}
+      </div>
+      <div>
+        Topics:
+        {config.config.topics.map((t) => (
+          <div key={`visual-config-topic-${t.name}`}>{JSON.stringify(t)}</div>
+        ))}
+      </div>
+      <div>
+        ConsumerGroups:
+        {config.config.consumerGroups.map((c) => (
+          <div key={`visual-config-consumergroup-${c.name}`}>
+            {JSON.stringify(c)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- services/monarch-ui/src/redux/reducers/existingResources.js
+++ services/monarch-ui/src/redux/reducers/existingResources.js
@@ -4,16 +4,29 @@ const initialState = {
   loaded: false,
 };
 
+const toTopic = (t, extra = {}) => ({ name: t.name, ...extra });
+
+const toConsumerGroup = (extra = {}) => (c) => ({ name: c.name, ...extra });
+
 export default function (state = initialState, action) {
   switch (action.type) {
     case REFRESH_STATE: {
+      const topicProducers = new Map();
+      action.payload.services.forEach((s) =>
+        s.producesTo.forEach((t) => topicProducers.set(t, s.name))
+      );
       return {
         config: {
           services: action.payload.services.map((s) => ({ name: s.name })),
-          topics: action.payload.topics.map((s) => ({ name: s.name })),
-          consumerGroups: action.payload.consumerGroups.map((s) => ({
-            name: s.name,
-          })),
+          topics: action.payload.topics.map((t) =>
+            toTopic(t, { producedToBy: topicProducers.get(t.name) })
+          ),
+          consumerGroups: [
+            ...action.payload.services.flatMap((s) =>
+              s.consumerGroups.map(toConsumerGroup({ ownedBy: s.name }))
+            ),
+            ...action.payload.consumerGroups.map(toConsumerGroup()),
+          ],
         },
         loaded: true,
       };
