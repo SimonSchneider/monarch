595053284b69b4641a676135d3160489e4e465db
Simon Schneider <dev@simonschneider.org>
2020-07-11 13:02:48 +0200

    wip

--- services/prom-mock/src/app.js
+++ services/prom-mock/src/app.js
@@ -10,35 +10,70 @@ app.use(bodyParser.raw());
 
 const answer = (result) => ({ data: { result } });
 
-const topics = () => stub.topics.map(
-  t => ({ metric: { topic: t.name }, value: [0, t.rate] }))
+const topics = () =>
+  stub.topics.map((t) => ({ metric: { topic: t.name }, value: [0, t.rate] }));
 
-const getCg = (key, { mod = (name, r) => r } = {}) => stub.consumerGroups.flatMap(
-  cg => cg.topics.map(t => ({ metric: { topic: t.name, consumergroup: cg.name }, value: [0, mod(cg.name, t[key])] }))
-);
+const getCg = (key, { mod = (name, r) => r } = {}) =>
+  stub.consumerGroups.flatMap((cg) =>
+    cg.topics.map((t) => ({
+      metric: { topic: t.name, consumergroup: cg.name },
+      value: [0, mod(cg.name, t[key])],
+    }))
+  );
 
-const getService = (key) => stub.services.flatMap(
-  s => (s.metrics ? { metric: { service: s.name }, value: [0, s.metrics.requests[key]] } : [])
-)
+const getService = (key) =>
+  stub.services.flatMap((s) =>
+    s.metrics
+      ? { metric: { service: s.name }, value: [0, s.metrics.requests[key]] }
+      : []
+  );
 
-function get(query, state) {
-  const lagMod = (state === "warn" || state === "crit") ? { mod: (name, l) => name === "service-8-consumer" ? l + 200000 : name === "service-4-consumer" ? l + 30000000 : l } : {};
-  const rateMod = state === "crit" ? { mod: (name, r) => name === "service-8-consumer" ? 10 : r } : {};
+async function get(query, state) {
+  const lagMod =
+    state === "warn" || state === "crit"
+      ? {
+          mod: (name, l) =>
+            name === "service-8-consumer"
+              ? l + 200000
+              : name === "service-4-consumer"
+              ? l + 30000000
+              : l,
+        }
+      : {};
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const rateMod =
+    state === "crit"
+      ? { mod: (name, r) => (name === "service-8-consumer" ? 10 : r) }
+      : {};
   switch (query) {
-    case "topic": return topics();
-    case "cgLag": return getCg('lag', lagMod);
-    case "cgRate": return getCg('rate', rateMod);
-    case "serviceRate": return getService('rate');
-    case "serviceError": return getService('errors');
-    case "serviceDuration": return getService('duration');
-    default: return "bad";
+    case "topic":
+      return topics();
+    case "cgLag":
+      return getCg("lag", lagMod);
+    case "cgRate":
+      return getCg("rate", rateMod);
+    case "serviceRate":
+      return getService("rate");
+    case "serviceError":
+      return getService("errors");
+    case "serviceDuration":
+      return getService("duration");
+    default:
+      return "bad";
   }
 }
 
 const states = ["good", "warn", "crit"];
 
-app.get(
-  "/api/v1/query",
-  (req, res) => res.json(answer(get(req.query.query, states[Math.floor(Math.random() * states.length)]))));
+app.get("/api/v1/query", async (req, res) =>
+  res.json(
+    answer(
+      await get(
+        req.query.query,
+        states[Math.floor(Math.random() * states.length)]
+      )
+    )
+  )
+);
 
 module.exports = app;
